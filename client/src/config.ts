4a2b0ed7e9d374f07ff97c6be30ed1dd1af6f889
Kendy JEAN-MARIE <kendyjm@protonmail.com>
2020-09-11 22:08:48 +0200

    set apiId and Auth0 parameters for submission

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'vuubvq4uvc'
+const region = 'eu-west-3'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-ut6kmzhz.eu.auth0.com',            // Auth0 domain
+  clientId: 'XwbiAcrbUeQO8Y6LKga3PUHd8abSYa6c',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
